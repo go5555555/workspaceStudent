fcd9129338abd28f53b321a9813dd51679357cb8
go5555555 <tiita0809@icloud.com>
2025-07-25 16:46:23 +0900

    生徒CRUDをREST化(一通り完成)

--- webapp/src/main/java/com/example/webapp/dto/OneStudentDTO.java
+++ webapp/src/main/java/com/example/webapp/dto/OneStudentDTO.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OneStudentDTO {
 	
+	String message;
+	
 	private Integer id;
 	
 	private String studentName;
@@ -17,6 +19,11 @@ public class OneStudentDTO {
 	
 	private Boolean IsNew; 
 	
-	
+	public OneStudentDTO(String message,Integer id,String studentName,Integer grade){
+		this.message = message;
+		this.id = id;
+		this.studentName = studentName;
+		this.grade = grade;
+	}
 
 }
--- webapp/src/main/java/com/example/webapp/helper/StudentHelper.java
+++ webapp/src/main/java/com/example/webapp/helper/StudentHelper.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 import com.example.webapp.entity.Student;
 import com.example.webapp.form.StudentForm;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class StudentHelper {
+
 	//生徒登録フォームを生徒エンティティに変換
 	public Student toEntityFromForm(StudentForm form) {
 		Student entity = new Student();
@@ -16,18 +20,18 @@ public class StudentHelper {
 		return entity;
 	}
 
-	public void sanitizeForm(StudentForm form) {
-		if(form.getStudentName() == null || form.getStudentName().isEmpty()) {
+	public void replaceEmptyFieldsWithNull(StudentForm form) {
+		if (form.getStudentName() == null || form.getStudentName().isEmpty()) {
 			form.setStudentName(null);
 		}
-		if(form.getGrade() == null || form.getGrade().equals(0)) {
+		if (form.getGrade() == null || form.getGrade().equals(0)) {
 			form.setGrade(null);
 		}
 	}
-	
+
 	public void inNull(StudentForm form) {
 		form.setStudentName(null);
 		form.setGrade(null);
 	}
-	
+
 }
